29c03357b095b4ecd1898960e8689ad41aac462d
carlosnalpizar <carlosnalpizar@icloud.com>
2024-07-02 10:20:45 -0600

    creacion y consumo de api

--- servidordunamis/src/app.js
+++ servidordunamis/src/app.js
@@ -1,8 +1,17 @@
+
 import express from 'express';
-import router from '../src/controladores/control.routes.js' // Nota: usa `default` en la importación
+import cors from 'cors';
+import router from '../src/controladores/control.routes.js'; // Asegúrate de usar `default` en la importación
+
 
 const app = express();
-app.use(express.json())
+
+app.use(cors());
+
+
+app.use(express.json());
+
+
 app.use(router);
 
 export default app;
